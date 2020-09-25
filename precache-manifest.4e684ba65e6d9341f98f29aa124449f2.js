70ef371757a8e854847894fbe1bcc24a5395ce60
Lincoln Adams <lincolnj.adams@gmail.com>
2020-09-25 00:33:26 -0500

    Updates

---
+++ precache-manifest.4e684ba65e6d9341f98f29aa124449f2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1eeae382731ce2c859ef27e029978b09",
+    "url": "/name-spinner/index.html"
+  },
+  {
+    "revision": "c0c21b67f83f10482b18",
+    "url": "/name-spinner/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "4ffaea547448a625aafd",
+    "url": "/name-spinner/static/js/2.1677f78b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/name-spinner/static/js/2.1677f78b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c0c21b67f83f10482b18",
+    "url": "/name-spinner/static/js/main.ee74a7b6.chunk.js"
+  },
+  {
+    "revision": "06b90fa5af5d2e8e1dbb",
+    "url": "/name-spinner/static/js/runtime-main.acb5fd8d.js"
+  }
+]);
